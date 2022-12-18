4ed831d8c5b830afa2f8af6b3c5622022a40dce4
thecyborganizer <3578699+thecyborganizer@users.noreply.github.com>
2022-12-18 06:58:43 -0500

    day11

--- 2022/day11.py
+++ 2022/day11.py
@@ -20,6 +20,7 @@ class monkey:
         self.testDivisor = testDivisor
         self.trueTarget = trueTarget
         self.falseTarget = falseTarget
+        self.itemsAsModuluses = []
     def __str__(self):
         return "Monkey {}:\n  Starting items: {}\n    Operation: {}\n    Test: {}\nTrue:{}\nFalse:{}\n".format(self.number, self.items, self.operation, self.testDivisor, self.trueTarget, self.falseTarget)
     def parseMonkey(monkeyText):
@@ -40,24 +41,45 @@ class monkey:
         return monkey(number, items, operation, testDivisor, trueTarget, falseTarget)
 
 monkeys = []
-with open("day11testinput.txt") as f:
+with open("day11input.txt") as f:
     lines = [x.strip() for x in f.readlines()]
     for i in range(0, len(lines), 7):
         monkeys.append(monkey.parseMonkey(lines[i:i+6]))
-moreMonkeys = [m for m in monkeys]
-for r in range(600):
+# for r in range(600):
+#     for m in monkeys:
+#         while len(m.items) > 0:
+#             m.inspectionCount = m.inspectionCount + 1
+#             item = m.items.pop(0)
+#             item = m.operation(item)
+#             if item % m.testDivisor == 0:
+#                 monkeys[m.trueTarget].items.append(item)
+#             else:
+#                 monkeys[m.falseTarget].items.append(item)
+
+# monkeys.sort(key=lambda m: m.inspectionCount, reverse=True)
+# print([m.inspectionCount for m in monkeys])
+# print(reduce(mul, [m.inspectionCount for m in monkeys[0:2]], 1))
+
+divisors = [m.testDivisor for m in monkeys]
+
+for m in monkeys:
+    for i in m.items:
+        dictionary = {}
+        for d in divisors:
+            dictionary[d] = i % d
+        m.itemsAsModuluses.append(dictionary)
+
+for r in range(10000):
     for m in monkeys:
-        while len(m.items) > 0:
+        while len(m.itemsAsModuluses) > 0:
             m.inspectionCount = m.inspectionCount + 1
-            item = m.items.pop(0)
-            item = m.operation(item)
-            if item % m.testDivisor == 0:
-                monkeys[m.trueTarget].items.append(item)
+            item = m.itemsAsModuluses.pop(0)
+            for d in divisors:
+                item[d] = m.operation(item[d]) % d
+            if item[m.testDivisor] == 0:
+                monkeys[m.trueTarget].itemsAsModuluses.append(item)
             else:
-                monkeys[m.falseTarget].items.append(item)
+                monkeys[m.falseTarget].itemsAsModuluses.append(item)
 
 monkeys.sort(key=lambda m: m.inspectionCount, reverse=True)
-print([m.inspectionCount for m in monkeys])
 print(reduce(mul, [m.inspectionCount for m in monkeys[0:2]], 1))
-
-monkeys = moreMonkeys
