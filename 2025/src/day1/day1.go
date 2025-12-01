083f789be97d2a6a7ee7501e89851f35a2f4880b
thecyborganizer <3578699+thecyborganizer@users.noreply.github.com>
2025-12-01 12:18:10 -0500

    2025 day 1

---
+++ 2025/src/day1/day1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"readline"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	lines := readline.Readlines("input.txt")
+	//lines := readline.Readlines("test.txt")
+
+	re := regexp.MustCompile(`([LR])(\d+)`)
+	rot := 50
+	pointCount := 0
+	clickCount := 0
+	for _, l := range lines {
+		sm := re.FindStringSubmatch(l)
+		dir := sm[1]
+		val, _ := strconv.Atoi(sm[2])
+		for i := 0; i < val; i++ {
+			if dir == "L" {
+				rot -= 1
+			} else {
+				rot += 1
+			}
+			if i != val-1 && rot%100 == 0 {
+				clickCount += 1
+			}
+		}
+		rot = (rot%100 + 100) % 100
+		if rot == 0 {
+			pointCount += 1
+		}
+	}
+	fmt.Printf("pt1: %d\n", pointCount)
+	fmt.Printf("pt2: %d\n", clickCount+pointCount)
+}
